5a8736a99cb9160020cb5048326719d992c9cfc3
Ryan Hull <ryandelonhull@gmail.com>
2020-11-17 01:27:54 -0500

    add header and footer functionality and to app.js

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import react from "react";
+import "./footer.css";
+
+function Footer() {
+  return(
+        <footer className="footer">
+            <h2>"Enjoy every sandwich" - Warren Zevon</h2>
+        </footer>
+  )  
+}
+
+export default Footer;
